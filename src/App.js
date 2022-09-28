6146fef0bf9befe5df3a21bddefc7b6cac3cc95d
rdininno <robertdininno@Roberts-MacBook-Pro.local>
2022-09-28 15:43:24 -0700

    create and import row component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>netflix clone</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNextlixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
+  );
 }
 
 export default App;
