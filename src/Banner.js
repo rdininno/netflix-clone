105ddacc1e12596152fd234bde2055ed8925986a
rdininno <robertdininno@Roberts-MacBook-Pro.local>
2022-11-17 16:01:27 -0800

    add banner compenent and start logic to request and display movie

---
+++ src/Banner.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNextlixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <header>
+      {/* header */}
+      {/* div > 2 buttons */}
+      {/* description */}
+    </header>
+  );
+}
+
+export default Banner;
