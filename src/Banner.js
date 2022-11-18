488e88968c004e948c1725996ee1cee9d1e840b6
rdininno <robertdininno@Roberts-MacBook-Pro.local>
2022-11-17 16:27:10 -0800

    truncate description

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -18,6 +19,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -29,15 +34,16 @@ function Banner() {
         backgroundPostition: "center center",
       }}
     >
-      <div className="banner-content">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
-      <div className="banner_buttons">
-        <button className="banner_button">Play</button>
-        <button className="banner_button">My List</button>
-      </div>
-
-      <h1 className="banner_description">{movie?.overview}</h1>
     </header>
   );
 }
